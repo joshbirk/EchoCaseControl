2d84d8fd3087bb9959b184d53a6e80068993e605
joshbirk <joshua.birk@gmail.com>
2015-07-08 14:47:43 -0500

    working with post

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ var port = process.env.PORT || 8080
 
 // Route request and response ends up here.
 function route_alexa(req, res) {
-   console.log(req);
    if(req.body == null) {
         return res.jsonp({message: 'no post body found'});
    }
-   // Grab the necessary values from the Echo request.
    alexa.launchRequest(req.body);
-   // Store the session and/or user data
-
-   // Respond to the Echo
    alexa.response('Welcome to my app, you can say things like this or that', {
      title: 'Launch Card Title',
      subtitle: 'Launch Card Subtitle',
      content: 'Launch Card Content'
    }, false, function (error, response) {
      if(error) {
+       console.log({message: error});
        return res.status(400).jsonp({message: error});
      }
      return res.jsonp(response);
    });
 };
 
+
+app.get('/', function (req, res) {
+  res.jsonp({status: 'running'});
+});
 
 app.post('/echo', function (req, res) {
   route_alexa(req,res);
