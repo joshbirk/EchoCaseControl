e995176f87b8f5dd01d16c40f5a5389a986f2e8f
joshbirk <joshua.birk@gmail.com>
2015-07-08 15:31:59 -0500

    working with post

--- index.js
+++ index.js
@@ -14,18 +14,35 @@ function route_alexa(req, res) {
    }
    console.log('forming alexa response');
    alexa.launchRequest(req.body);
-   alexa.response('Hello World', {
-     title: 'Heroku',
-     subtitle: 'Hello World',
-     content: '<h1>Hello World!</h1>',
-     shouldEndSession: true
-   }, false, function (error, response) {
-     if(error) {
-       console.log({message: error});
-       return res.status(400).jsonp({message: error});
-     }
-     return res.jsonp(response);
-   });
+   if(req.body.intentName == 'GetLatestCases') {
+         alexa.response('I am get your latest cases', {
+           title: 'Heroku',
+           subtitle: 'Latest Cases',
+           content: 'List goes here',
+           shouldEndSession: true
+         }, false, function (error, response) {
+           if(error) {
+             console.log({message: error});
+             return res.status(400).jsonp({message: error});
+           }
+           return res.jsonp(response);
+         });
+      }
+
+       if(req.body.intentName == 'GetLatestCase') {
+         alexa.response('Here is your latest case', {
+           title: 'Heroku',
+           subtitle: 'Latest Case',
+           content: 'Information goes here',
+           shouldEndSession: true
+         }, false, function (error, response) {
+           if(error) {
+             console.log({message: error});
+             return res.status(400).jsonp({message: error});
+           }
+           return res.jsonp(response);
+         });
+      }
 };
 
 
