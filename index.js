121bd62580b647f5fb2280344b1fd824d6339038
joshbirk <joshua.birk@gmail.com>
2015-07-08 16:38:14 -0500

    attempting salesforce callout

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function route_alexa_intent(req, res) {
    alexa.intentRequest(req.body);
    if(alexa.intentName == 'GetLatestCases') {
    
-      org.query({ query: 'SELECT ID, Subject FROM Cases ORDERBY CreatedDate ASC LIMIT 5', oauth: org.oauth }, 
+      org.query({ query: 'SELECT ID, Subject FROM Case ORDER BY CreatedDate ASC LIMIT 5', oauth: org.oauth }, 
         function(err, records){
             if(err) {
               console.log(err);
